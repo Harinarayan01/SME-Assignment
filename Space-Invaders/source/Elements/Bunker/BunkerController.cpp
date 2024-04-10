136244c12596610f83d2e595c4160ab4fb0caac5
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-10 20:09:51 +0530

    Update BunkerController.cpp

--- Space-Invaders/source/Elements/Bunker/BunkerController.cpp
+++ Space-Invaders/source/Elements/Bunker/BunkerController.cpp
@@ -5,29 +5,29 @@
 
 namespace Element
 {
-	namespace Bunker
-	{
-		using namespace Entity;
-		using namespace Global;
+    namespace Bunker
+    {
+        using namespace Entity;
+        using namespace Global;
 
-		BunkerController::BunkerController() { bunker_view = new BunkerView(); }
+        BunkerController::BunkerController() : bunker_view(std::make_unique<BunkerView>()) {}
 
-		BunkerController::~BunkerController() { delete(bunker_view); }
+        BunkerController::~BunkerController() {}
 
-		void BunkerController::initialize(BunkerData data)
-		{
-			bunker_data = data;
-			bunker_view->initialize(this); 
-		}
+        void BunkerController::initialize(const BunkerData& data)
+        {
+            bunker_data = data;
+            bunker_view->initialize(this);
+        }
 
-		void BunkerController::update() { bunker_view->update(); }
+        void BunkerController::update() { bunker_view->update(); }
 
-		void BunkerController::render() { bunker_view->render(); }
+        void BunkerController::render() { bunker_view->render(); }
 
-		const sf::Sprite& BunkerController::getColliderSprite() { return bunker_view->getBunkerSprite(); }
+        const sf::Sprite& BunkerController::getColliderSprite() const { return bunker_view->getBunkerSprite(); }
 
-		void BunkerController::onCollision(ICollider* other_collider) { }
+        void BunkerController::onCollision(ICollider* other_collider) { }
 
-		sf::Vector2f BunkerController::getBunkerPosition() { return bunker_data.position; }
-	}
+        sf::Vector2f BunkerController::getBunkerPosition() const { return bunker_data.position; }
+    }
 }
