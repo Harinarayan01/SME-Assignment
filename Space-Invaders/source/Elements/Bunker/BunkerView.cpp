a5929fc636059c11ea587672d8224737afd1bed9
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-10 20:13:06 +0530

    Update BunkerView.cpp

--- Space-Invaders/source/Elements/Bunker/BunkerView.cpp
+++ Space-Invaders/source/Elements/Bunker/BunkerView.cpp
@@ -3,53 +3,40 @@
 #include "../../header/Graphics/GraphicService.h"
 #include "../../header/Global/Config.h"
 #include "../../header/Elements/Bunker/BunkerController.h"
+#include "../../header/UI/ImageView.h" // Assuming ImageView is part of UI namespace
 
 namespace Element
 {
-	namespace Bunker
-	{
-		using namespace Global;
-		using namespace Graphics;
-		using namespace UI::UIElement;
+    namespace Bunker
+    {
+        using namespace Global;
+        using namespace Graphics;
+        using namespace UI;
 
-		BunkerView::BunkerView() { createUIElements(); }
+        BunkerView::BunkerView() : bunker_image(new ImageView()) { createUIElements(); }
 
-		BunkerView::~BunkerView() { destroy(); }
+        BunkerView::~BunkerView() { delete bunker_image; }
 
-		void BunkerView::initialize(BunkerController* controller)
-		{
-			bunker_controller = controller;
-			initializeImage();
-		}
+        void BunkerView::initialize(BunkerController* controller)
+        {
+            bunker_controller = controller;
+            initializeImage();
+        }
 
-		void BunkerView::createUIElements()
-		{
-			bunker_image = new ImageView();
-		}
+        void BunkerView::createUIElements() {}
 
-		void BunkerView::initializeImage()
-		{
-			bunker_image->initialize(Config::bunker_texture_path, bunker_sprite_width, bunker_sprite_height, bunker_controller->getBunkerPosition());
-		}
+        void BunkerView::initializeImage()
+        {
+            bunker_image->initialize(Config::bunker_texture_path, bunker_sprite_width, bunker_sprite_height, bunker_controller->getBunkerPosition());
+        }
 
-		void BunkerView::update()
-		{
-			bunker_image->update();
-		}
+        void BunkerView::update() { bunker_image->update(); }
 
-		void BunkerView::render()
-		{
-			bunker_image->render();
-		}
+        void BunkerView::render() { bunker_image->render(); }
 
-		const sf::Sprite& BunkerView::getBunkerSprite() 
-		{
-			return bunker_image->getSprite();
-		}
-
-		void BunkerView::destroy()
-		{
-			delete(bunker_image);
-		}
-	}
+        const sf::Sprite& BunkerView::getBunkerSprite() 
+        {
+            return bunker_image->getSprite();
+        }
+    }
 }
