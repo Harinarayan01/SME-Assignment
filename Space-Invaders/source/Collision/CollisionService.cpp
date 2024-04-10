b87bf9f331a316b6acd26dfcf7ca50cc4ffff8e9
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-10 20:17:04 +0530

    Update CollisionService.cpp

--- Space-Invaders/source/Collision/CollisionService.cpp
+++ Space-Invaders/source/Collision/CollisionService.cpp
@@ -1,66 +1,68 @@
 #include "../../header/Collision/CollisionService.h"
 #include "../../header/Collision/ICollider.h"
+#include <SFML/Graphics.hpp> // Include the appropriate header for sf::Sprite
 
 namespace Collision
 {
-	CollisionService::CollisionService() { }
+    CollisionService::CollisionService() {}
 
-	CollisionService::~CollisionService() { }
+    CollisionService::~CollisionService() {}
 
-	void CollisionService::initialize() { }
+    void CollisionService::initialize() {}
 
-	void CollisionService::update()
-	{
-		processCollision();
-	}
+    void CollisionService::update()
+    {
+        processCollision();
+    }
 
-	void CollisionService::processCollision()
-	{
-		for (int i = 0; i < collider_list.size(); i++)
-		{
-			for (int j = i + 1; j < collider_list.size(); j++)
-			{
-				doCollision(i, j);
-			}
-		}
-	}
+    void CollisionService::processCollision()
+    {
+        for (size_t i = 0; i < collider_list.size() - 1; ++i)
+        {
+            for (size_t j = i + 1; j < collider_list.size(); ++j)
+            {
+                doCollision(i, j);
+            }
+        }
+    }
 
-	void CollisionService::doCollision(int index_i, int index_j)
-	{
-		if (collider_list[index_i]->getCollisionState() == CollisionState::DISABLED ||
-			collider_list[index_j]->getCollisionState() == CollisionState::DISABLED) return;
+    void CollisionService::doCollision(size_t index_i, size_t index_j)
+    {
+        if (index_i >= collider_list.size() || index_j >= collider_list.size())
+            return;
 
-		if (hasCollisionOccurred(index_i, index_j))
-		{
-			if (areActiveColliders(index_i, index_j)) 
-				collider_list[index_i]->onCollision(collider_list[index_j]);
+        ICollider* collider_i = collider_list[index_i];
+        ICollider* collider_j = collider_list[index_j];
 
-			if (areActiveColliders(index_i, index_j)) 
-				collider_list[index_j]->onCollision(collider_list[index_i]);
-		}
-	}
+        if (collider_i == nullptr || collider_j == nullptr)
+            return;
 
-	bool CollisionService::hasCollisionOccurred(int index_i, int index_j)
-	{
-		const sf::Sprite& collider_one_sprite = collider_list[index_i]->getColliderSprite();
-		const sf::Sprite& collider_two_sprite = collider_list[index_j]->getColliderSprite();
+        if (collider_i->getCollisionState() == CollisionState::DISABLED ||
+            collider_j->getCollisionState() == CollisionState::DISABLED)
+            return;
 
-		return collider_one_sprite.getGlobalBounds().intersects(collider_two_sprite.getGlobalBounds());
-	}
+        if (hasCollisionOccurred(index_i, index_j))
+        {
+            collider_i->onCollision(collider_j);
+            collider_j->onCollision(collider_i);
+        }
+    }
 
-	bool CollisionService::areActiveColliders(int index_i, int index_j)
-	{
-		return (index_i < collider_list.size() && index_j < collider_list.size() &&
-			collider_list[index_i] != nullptr && collider_list[index_j] != nullptr);
-	}
+    bool CollisionService::hasCollisionOccurred(size_t index_i, size_t index_j)
+    {
+        const sf::Sprite& collider_one_sprite = collider_list[index_i]->getColliderSprite();
+        const sf::Sprite& collider_two_sprite = collider_list[index_j]->getColliderSprite();
 
-	void CollisionService::addCollider(ICollider* collider)
-	{
-		collider_list.push_back(collider);
-	}
+        return collider_one_sprite.getGlobalBounds().intersects(collider_two_sprite.getGlobalBounds());
+    }
 
-	void CollisionService::removeCollider(ICollider* collider)
-	{
-		collider_list.erase(std::remove(collider_list.begin(), collider_list.end(), collider), collider_list.end());
-	}
+    void CollisionService::addCollider(ICollider* collider)
+    {
+        collider_list.push_back(collider);
+    }
+
+    void CollisionService::removeCollider(ICollider* collider)
+    {
+        collider_list.erase(std::remove(collider_list.begin(), collider_list.end(), collider), collider_list.end());
+    }
 }
