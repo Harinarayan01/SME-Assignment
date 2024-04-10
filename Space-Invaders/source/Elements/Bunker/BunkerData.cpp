c7f17f667076130d036d6e586f12914d7fdb3edc
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-10 20:11:35 +0530

    Update BunkerData.cpp

--- Space-Invaders/source/Elements/Bunker/BunkerData.cpp
+++ Space-Invaders/source/Elements/Bunker/BunkerData.cpp
@@ -1,14 +1,15 @@
 #include "../../header/Elements/Bunker/BunkerData.h"
+#include <SFML/System/Vector2.hpp> // Include the appropriate header for sf::Vector2f
 
 namespace Element
 {
-	namespace Bunker
-	{
-		BunkerData::BunkerData() { }
+    namespace Bunker
+    {
+        BunkerData::BunkerData() { }
 
-		BunkerData::BunkerData(sf::Vector2f position)
-		{
-			this->position = position;
-		}
-	}
+        BunkerData::BunkerData(sf::Vector2f position)
+        {
+            this->position = position;
+        }
+    }
 }
