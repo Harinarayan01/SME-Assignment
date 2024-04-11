c569f0fc07cb7ea77c0f2d8bf03914a5cfba8f58
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-11 09:30:26 +0530

    Update HighScore.cpp

--- Space-Invaders/source/Gameplay/HighScore.cpp
+++ Space-Invaders/source/Gameplay/HighScore.cpp
@@ -1,35 +1,42 @@
 #include "../../header/Gameplay/HighScore.h"
 #include <fstream>
+#include <iostream>
 
 namespace Gameplay
 {
-	void HighScore::saveHighScore(const HighScoreData& highScore)
-	{
-		std::ofstream high_score_file("highscore.txt");
+    void HighScore::saveHighScore(const HighScoreData& highScore)
+    {
+        std::ofstream high_score_file("highscore.txt");
 
-		if (high_score_file.is_open())
-		{
-			high_score_file << highScore.player_name << " " << highScore.score << std::endl;
-			high_score_file.close();
-		}
-		else printf("Failed to open highscore file for writing.");
-	}
+        if (high_score_file.is_open())
+        {
+            high_score_file << highScore.player_name << " " << highScore.score << std::endl;
+            high_score_file.close();
+        }
+        else 
+        {
+            std::cerr << "Failed to open highscore file for writing." << std::endl;
+        }
+    }
 
-	HighScoreData HighScore::loadHighScore()
-	{
-		HighScoreData highScore;
-		highScore.player_name = "Outscal";
-		highScore.score = 0;
+    HighScoreData HighScore::loadHighScore()
+    {
+        HighScoreData highScore;
+        highScore.player_name = "Outscal";
+        highScore.score = 0;
 
-		std::ifstream high_score_file("highscore.txt");
+        std::ifstream high_score_file("highscore.txt");
 
-		if (high_score_file.is_open()) 
-		{
-			high_score_file >> highScore.player_name >> highScore.score;
-			high_score_file.close();
-		}
-		else  printf("Failed to open highscore file for writing.");
+        if (high_score_file.is_open()) 
+        {
+            high_score_file >> highScore.player_name >> highScore.score;
+            high_score_file.close();
+        }
+        else 
+        {
+            std::cerr << "Failed to open highscore file for reading." << std::endl;
+        }
 
-		return highScore;
-	}
+        return highScore;
+    }
 }
