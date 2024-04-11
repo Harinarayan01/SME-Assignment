9fbd21b286a77b1419328686af78fa091e1e6748
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-11 09:26:58 +0530

    Update GameplayController.cpp

--- Space-Invaders/source/Gameplay/GameplayController.cpp
+++ Space-Invaders/source/Gameplay/GameplayController.cpp
@@ -3,13 +3,13 @@
 
 namespace Gameplay
 {
-	GameplayController::GameplayController() { gameplay_view = new GameplayView(); }
+    GameplayController::GameplayController() : gameplay_view(new GameplayView()) {}
 
-	GameplayController::~GameplayController() { delete (gameplay_view); }
+    GameplayController::~GameplayController() { delete gameplay_view; }
 
-	void GameplayController::initialize() { gameplay_view->initialize(); }
+    void GameplayController::initialize() { gameplay_view->initialize(); }
 
-	void GameplayController::update() { gameplay_view->update(); }
+    void GameplayController::update() { gameplay_view->update(); }
 
-	void GameplayController::render() { gameplay_view->render(); }
+    void GameplayController::render() { gameplay_view->render(); }
 }
