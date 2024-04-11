6b8efb37728b212e86cb623f2c54a55b90ab6ec4
Hari Narayan <113297223+Harinarayan01@users.noreply.github.com>
2024-04-11 09:27:58 +0530

    Update GameplayService.cpp

--- Space-Invaders/source/Gameplay/GameplayService.cpp
+++ Space-Invaders/source/Gameplay/GameplayService.cpp
@@ -4,25 +4,25 @@
 
 namespace Gameplay
 {
-	using namespace Global;
-	using namespace Player;
-	using namespace Enemy;
-	using namespace Element;
+    using namespace Global;
+    using namespace Player;
+    using namespace Enemy;
+    using namespace Element;
 
-	GameplayService::GameplayService() { gameplay_controller = new GameplayController(); }
+    GameplayService::GameplayService() : gameplay_controller(new GameplayController()) {}
 
-	GameplayService::~GameplayService() { delete (gameplay_controller); }
+    GameplayService::~GameplayService() { delete gameplay_controller; }
 
-	void GameplayService::initialize() { gameplay_controller->initialize(); }
+    void GameplayService::initialize() { gameplay_controller->initialize(); }
 
-	void GameplayService::update() { gameplay_controller->update(); }
+    void GameplayService::update() { gameplay_controller->update(); }
 
-	void GameplayService::render() { gameplay_controller->render(); }
+    void GameplayService::render() { gameplay_controller->render(); }
 
-	void GameplayService::restart()
-	{
-		ServiceLocator::getInstance()->getPlayerService()->reset();
-		ServiceLocator::getInstance()->getEnemyService()->reset();
-		ServiceLocator::getInstance()->getElementService()->reset();
-	}
+    void GameplayService::restart()
+    {
+        ServiceLocator::getInstance()->getPlayerService()->reset();
+        ServiceLocator::getInstance()->getEnemyService()->reset();
+        ServiceLocator::getInstance()->getElementService()->reset();
+    }
 }
